0760820cab1d86bcc489b6f74eebb434b31aff28
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2021-09-05 19:48:58 -0400

    Updated the App scene contents.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jon Hargreaves - Design generalist
+          Jon Hargreaves
         </p>
-        <a
-          className="App-link"
-          href="https://jonhargreaves.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Design
-        </a>
+        <p>
+          Design generalist
+        </p>
       </header>
     </div>
   );
