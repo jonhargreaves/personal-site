d6778a67108ef8da6c062434fc82865818a3344c
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2021-09-05 13:06:58 -0400

    Built the React app finally.

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jon Hargreaves - Design generalist
+        </p>
+        <a
+          className="App-link"
+          href="https://jonhargreaves.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Design
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
