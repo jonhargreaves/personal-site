87af48e1401a048efbb95e369ab01756704944d9
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2021-09-11 22:08:31 -0400

    All content has been added.

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,101 @@ function App() {
         <div className="app-section__vertical-spacing" />
         <div className="app-section__yellow-divider" />
         <div className="app-section__vertical-spacing" />
+        <div className="app-section__row">
+          <div className="app-section__yellow-heading">
+            <h3>
+              What I'm doing
+            </h3>
+            <div className="app-section__horizontal-spacing" />
+          </div>
+          <div className="app-section__medium-text-column">
+            <p>
+              I'm a designer who works remotely as the Head of Design at Caretaker. We are attempting to disrupt the property management and rental industry with our software.
+              <br />
+              <br />
+              I’m currently not freelancing.
+              <br />
+              <br />
+              When not designing, I’m either researching fashion trends, getting tattoos, listening to house music or jazz, searching for weird vintage furniture, shooting film, riding my bike, or reading about mid century architecture.
+              <br />
+              <br />
+              Massimo and Lella Vignelli are my design heros.
+            </p>
+          </div>
+        </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__red-divider" />
         <div className="app-section__vertical-spacing" />
+        <div className="app-section__row">
+          <div className="app-section__red-heading">
+            <h3>
+              What I've done
+            </h3>
+            <div className="app-section__horizontal-spacing" />
+          </div>
+          <div className="app-section__medium-text-column">
+            <p>
+              I've designed and branded www.caretaker.com<br />
+              (a product for landlords)
+              <br />
+              <br />
+              I’ve designed and branded www.apartment.app<br />
+              (a Caretaker company for renters)
+              <br />
+              <br />
+              I've done a few small, personal design projects and developed a few rolls of film here. I started my career as an illustrator and brand designer, moved to product design and front end dev, and finally landed on being a design generalist.
+            </p>
+          </div>
+        </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__blue-divider" />
         <div className="app-section__vertical-spacing" />
+        <div className="app-section__row">
+          <div className="app-section__blue-heading">
+            <h3>
+              What I want to do
+            </h3>
+            <div className="app-section__horizontal-spacing" />
+          </div>
+          <div className="app-section__medium-text-column">
+            <p>
+              I'm working on starting a podcast about working remotely as a designer called Designers Drinking Coffee.
+              <br />
+              <br />
+              I want to collect rare, mid century/modern furniture, buy a mid century house in Virginia, and to design more than just things that live on the internet. I want to design homes, furniture, houseware, clothing, etc.
+              </p>
+          </div>
+        </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__white-divider" />
+        <div className="app-section__vertical-spacing" />
+        <div className="app-section__row">
+          <div className="app-section__white-heading">
+            <h3>
+              Some links
+            </h3>
+            <div className="app-section__horizontal-spacing" />
+          </div>
+          <div className="app-section__small-text-column">
+            <p>
+              If you want to talk, reach out to me here and I'll respond when I can.
+              <br />
+              <br />
+              For projects I’m working on, go here. For thoughts and rants I have, go here.
+            </p>
+          </div>
+          <div className="app-section__horizontal-spacing" />
+          <div className="app-section__small-text-column">
+            <p>
+              Special thanks to Pretend Foundry for the beautiful typefaces used on this site. Check them out here.
+            </p>
+          </div>
+        </div>
+        <div className="app-section__vertical-spacing" />
+        <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <h3>
           "If you can design one thing, you can design everything."
