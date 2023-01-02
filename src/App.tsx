ecee209d63c369b4bcc44a6f98e6432355232090
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2023-01-02 16:51:15 -0500

    BIG UPDATE
    Should have done this in more commits, my b lol.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Logo from './logo.svg';
 import './App.css';
 
 const META_TITLE = 'Jon Hargreaves | Design Generalist'
-const META_DESCRIPTION = 'Multi-disciplinary designer from Norfolk, VA | Head of Design at Caretaker'
-const DEFAULT_KEYWORDS = 'Jon Hargreaves, designer, frontend developer, Virginia'
+const META_DESCRIPTION = 'Multi-disciplinary designer from Norfolk, VA | Senior Product Designer at QuickNode'
+const DEFAULT_KEYWORDS = 'Jon Hargreaves, design, designer, frontend developer, brand systems, design systems, Virginia, Norfolk'
 const AUTHOR = 'Jon Hargreaves'
 
 function App() {
@@ -36,21 +37,22 @@ function App() {
         <div className="app-section__row">
           <div className="app-section__yellow-heading">
             <h3>
-              What I'm doing
+              Currently
             </h3>
-            <div className="app-section__horizontal-spacing" />
           </div>
-          <div className="app-section__medium-text-column">
+          <div className="app-section__horizontal-spacing" />
+          <div className="app-section__column">
             <p>
-              I'm currently working remotely as the Head of Design at <a href="https://caretaker.com/" className="app-section__yellow-link">Caretaker</a>. We are attempting to disrupt the rental housing industry with our property management software.
+              I'm working remotely as a Senior Product Designer at <a href="https://quicknode.com/" target="_blank" className="app-section__yellow-link">QuickNode</a>. We make blockchain development simple and Web3 is powered by QuickNode.
               <br />
               <br />
-              I’m currently not freelancing.
+              Freelancing status: <span>UNAVAILABLE</span>
               <br />
               <br />
-              When not designing, I’m researching fashion trends, getting tattoos, listening to house or jazz music, reading about mid century/modern furniture and architecutre, shooting 35mm film, or riding my fixie bike.
+              When I'm not designing, I&#39;m researching fashion trends, collecting modernist furniture, watching Formula 1, getting tattoos, listening to house or jazz music, learning about modernist architecutre, shooting 35mm film, or riding my fixie bike around Norfolk, VA.
             </p>
           </div>
+          <div className="app-section__horizontal-spacing" />
         </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
@@ -60,24 +62,45 @@ function App() {
         <div className="app-section__row">
           <div className="app-section__red-heading">
             <h3>
-              What I've done
+              Previously
             </h3>
-            <div className="app-section__horizontal-spacing" />
           </div>
-          <div className="app-section__medium-text-column">
+          <div className="app-section__horizontal-spacing" />
+          <div className="app-section__column">
             <p>
-              I've designed, branded, and coded <a href="https://caretaker.com/" className="app-section__red-link">www.caretaker.com</a><br />
-              <span>(a product for property owners)</span>
+              01/2022 - 05/2022
               <br />
+              <a href="https://icy.tools/" target="_blank" className="app-section__red-link">Icy Tools</a>&nbsp;<span>(NFT analytics tools)</span>
+              <ul>
+                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
+                <li>Managed the design system + brand system</li>
+                <li>Coded marketing page animations</li>
+                <li>Acquired by QuickNode</li>
+              </ul>
               <br />
-              I’ve designed, branded, and coded <a href="https://apartment.app/" className="app-section__red-link">www.apartment.app</a><br />
-              <span>(a Caretaker company for renters)</span>
+              11/2020 - 01/2022
               <br />
+              <a href="https://apartment.app/" target="_blank" className="app-section__red-link">Apartment App</a>&nbsp;<span>(a Caretaker company for renters)</span>
+              <ul>
+                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
+                <li>Managed the design system + brand system</li>
+                <li>Coded marketing pages, components, & animations</li>
+              </ul>
               <br />
-              I started my career as a freelance brand designer and illustrator. I was contracted and later hired by Caretaker (previously Flip) to do brand design, illustration, and product design.
-              As time went on I learned front end development and started maintaining Caretaker's design system. I've realized that I'm a Design Generalist: a designer with many focuses.
+              11/2016 - 01/2022
+              <br />
+              <a href="https://caretaker.com/" target="_blank" className="app-section__red-link">Caretaker</a>&nbsp;<span>(automated property management suite)</span>
+              <ul>
+                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
+                <li>Managed the design system + brand system</li>
+                <li>Coded marketing pages, components, & animations</li>
+              </ul>
+              <br />
+              My career started as a freelance brand designer and illustrator. I was contracted and later hired by Caretaker (previously Flip) as a brand and product designer.
+              As time went on, I learned front end development and started maintaining Caretaker's design system. I've realized that I'm a Design Generalist: a designer with many focuses.
             </p>
           </div>
+          <div className="app-section__horizontal-spacing" />
         </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
@@ -87,19 +110,23 @@ function App() {
         <div className="app-section__row">
           <div className="app-section__blue-heading">
             <h3>
-              What I want to do
+              Looking ahead
             </h3>
-            <div className="app-section__horizontal-spacing" />
           </div>
-          <div className="app-section__medium-text-column">
+          <div className="app-section__horizontal-spacing" />
+          <div className="app-section__column">
             <p>
-              I'm in the process of starting on a podcast about working remotely as a designer called Designers Drinking Coffee.
+              I want to continue learning, mentor up-and-coming designers, and design more than just things that live on the internet. The possiblity of designing homes, furniture, houseware, and clothing is very exciting to me.
               <br />
               <br />
-              I would like to collect mid century/modern furniture, buy a mid century house, and design more than just things that live on the internet. The possiblity of designing homes, furniture, houseware, and clothing is exciting to me.
-              Most of all, I want to explore design the way <a href="https://www.rit.edu/vignellicenter/vignelli-legacy" className="app-section__blue-link">Massimo and Lella Vignelli</a> did.
+              I would also love to continue growing my collection of modernist furniture, live in a mid century/modern home, find ways to build a more sustainable future with thoughtful design, and make good design accessible to everyone.
+              Most of all, I want to explore design the way <a href="https://www.rit.edu/vignellicenter/vignelli-legacy" target="_blank" className="app-section__blue-link">Massimo and Lella Vignelli</a> did.
+              <br />
+              <br />
+              I've never worked in an office and I don't intend to for the entirety of my career as a designer. I believe remote work is the future.
               </p>
           </div>
+          <div className="app-section__horizontal-spacing" />
         </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
@@ -111,27 +138,30 @@ function App() {
             <h3>
               Some links
             </h3>
-            <div className="app-section__horizontal-spacing" />
           </div>
-          <div className="app-section__small-text-column">
+          <div className="app-section__horizontal-spacing" />
+          <div className="app-section__column">
             <p>
-              If you want to talk, <a href="mailto:jhargreavesdesigns@gmail.com" className="app-section__white-link">message me</a> and I'll respond to you as soon as I can.
-              For recent projects I’ve worked on, go <a href="https://www.instagram.com/jonhargreaves/?hl=en" className="app-section__white-link">here</a>.
-              For rants and thoughts I have about design, go <a href="https://twitter.com/shml_onathan?lang=en" className="app-section__white-link">here</a>.
+              If you want to talk, are curious about remote work, or are interested in working together, <a href="mailto:jhargreavesdesigns@gmail.com" target="_blank" className="app-section__white-link">message me</a> and I'll respond to you as soon as I can.
+              For recent projects, thoughts I have about design, and insights on remote work go <a href="https://twitter.com/JonGHargreaves" target="_blank" className="app-section__white-link">here</a>.
+              <br />
+              <br />
+              A very special thanks to all of my wonderful friends who have helped me along my journey and to <a href="https://pretendfoundry.com/" target="_blank" className="app-section__white-link">Pretend Foundry</a> for the beautiful typefaces used on this website:
+              <ul>
+                <li><a href="https://pretendfoundry.com/broad" target="_blank" className="app-section__white-link">Broad Bold</a></li>
+                <li><a href="https://pretendfoundry.com/pinopolis" target="_blank" className="app-section__white-link">Pinopolis</a></li>
+              </ul>
             </p>
           </div>
           <div className="app-section__horizontal-spacing" />
-          <div className="app-section__small-text-column">
-            <p>
-              A very special thanks to <a href="https://pretendfoundry.com/" className="app-section__white-link">Pretend Foundry</a> for the beautiful typefaces used on this website: Broad Bold and Pinopolis.
-            </p>
-          </div>
         </div>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
+        <div className="app-section__white-divider" />
+        <div className="app-section__vertical-spacing" />
         <h3>
-          "If you can design one thing, you can design everything."
+          "Styles come and go. Good design is a language, not a style."
         </h3>
         <div className="app-section__vertical-spacing" />
         <h2>
@@ -140,7 +170,9 @@ function App() {
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
-        <p>This website was designed and coded by Jon Hargeaves © 2021</p>
+        <p><span>This website was designed and coded by Jon Hargeaves © 2022</span></p>
+        <div className="app-section__vertical-spacing" />
+        <img src={Logo} className="app-section__logo" alt="logo" />
       </div>
     </div>
   );
