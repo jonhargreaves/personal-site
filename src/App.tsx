a7e9e3e7e665f0cc427ff4b0bcc79e9b453ceb54
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2023-01-02 20:46:51 -0500

    Final updates for v2.

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,9 @@ function App() {
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <h3>
-          Design Generalist
+          Design Generalist:
+          <br />
+          <span>A designer with many focuses.</span>
         </h3>
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
@@ -47,13 +49,16 @@ function App() {
           <div className="app-section__horizontal-spacing" />
           <div className="app-section__column">
             <p>
-              I'm working remotely as a Senior Product Designer at <a href="https://quicknode.com/" target="_blank" className="app-section__yellow-link">QuickNode</a>. We make blockchain development simple and approachable for everyone. Web3 is powered by QuickNode.
+              I'm focused on branding, product design, design systems, front end development, and mentoring. I've never worked in an office and I intend to work remotely for my entire design career.
+              <br />
+              <br />
+              I'm working remotely as a Senior Product Designer at <a href="https://quicknode.com/" target="_blank" className="app-section__yellow-link">QuickNode</a>.
+              <br />
+              <br />
+              When I'm not designing, I&#39;m researching fashion trends, collecting modernist furniture, watching Formula 1, listening to jazz, learning about modernist architecutre, shooting 35mm film, or riding my fixie bike around Norfolk, VA.
               <br />
               <br />
               Freelancing status: <span>UNAVAILABLE</span>
-              <br />
-              <br />
-              When I'm not designing, I&#39;m researching fashion trends, collecting modernist furniture, watching Formula 1, getting tattoos, listening to house or jazz music, learning about modernist architecutre, shooting 35mm film, or riding my fixie bike around Norfolk, VA.
             </p>
           </div>
           <div className="app-section__horizontal-spacing" />
@@ -90,7 +95,7 @@ function App() {
               <br />
               <br />
               My career started as a freelance brand designer and illustrator. I was contracted and later hired by Caretaker (previously Flip) as a brand and product designer.
-              As time went on, I learned front end development and started maintaining Caretaker's design system. I've realized that I'm a Design Generalist: a designer with many focuses.
+              As time went on, I learned front end development and started maintaining Caretaker's design system. I realized later that I'm a Design Generalist.
             </p>
           </div>
           <div className="app-section__horizontal-spacing" />
@@ -109,14 +114,11 @@ function App() {
           <div className="app-section__horizontal-spacing" />
           <div className="app-section__column">
             <p>
-              I want to continue learning and design more than just things that live on the internet. The possiblity of designing homes, furniture, houseware, and clothing is very exciting to me.
+              I want to continue learning and design more than things that exist exclusively on the internet. The possiblity of designing homes, furniture, houseware, and clothing is very exciting to me.
               <br />
               <br />
               I would also love to continue growing my collection of modernist furniture, find ways to build a more sustainable future with thoughtful design, and make good design accessible to everyone.
               Most of all, I want to explore design the way <a href="https://www.rit.edu/vignellicenter/vignelli-legacy" target="_blank" className="app-section__blue-link">Massimo and Lella Vignelli</a> did.
-              <br />
-              <br />
-              I've never worked in an office and I intend to work remotely for my entire design career.
               </p>
           </div>
           <div className="app-section__horizontal-spacing" />
@@ -139,11 +141,8 @@ function App() {
               For recent projects, thoughts I have about design, and insights on remote work go <a href="https://twitter.com/JonGHargreaves" target="_blank" className="app-section__white-link">here</a>.
               <br />
               <br />
-              A very special thanks to all of my wonderful friends who have helped me along my journey and to <a href="https://pretendfoundry.com/" target="_blank" className="app-section__white-link">Pretend Foundry</a> for the beautiful typefaces used on this website:
-              <ul>
-                <li><a href="https://pretendfoundry.com/broad" target="_blank" className="app-section__white-link">Broad Bold</a></li>
-                <li><a href="https://pretendfoundry.com/pinopolis" target="_blank" className="app-section__white-link">Pinopolis</a></li>
-              </ul>
+              A very special thanks to all of my wonderful friends who have helped me along my journey and to <a href="https://pretendfoundry.com/" target="_blank" className="app-section__white-link">Pretend Foundry</a> for the beautiful typefaces used on this website:&nbsp;
+              <a href="https://pretendfoundry.com/broad" target="_blank" className="app-section__white-link">Broad Bold</a>&nbsp;&&nbsp;<a href="https://pretendfoundry.com/pinopolis" target="_blank" className="app-section__white-link">Pinopolis</a>.
             </p>
           </div>
           <div className="app-section__horizontal-spacing" />
@@ -163,7 +162,7 @@ function App() {
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
-        <p><span>This website was designed and coded by Jon Hargeaves © 2022</span></p>
+        <p><span>This website was designed and coded by Jon Hargeaves © 2023</span></p>
       </div>
     </div>
   );
