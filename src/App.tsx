3f052b015785de0a6e85bcfd4019bf67f5341210
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2023-01-02 19:56:23 -0500

    Updated copy and moved the logo.

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,10 @@ function App() {
       <div className="app-wrapper">
         <div className="app-section__white-divider" />
         <div className="app-section__vertical-spacing" />
+        <img src={Logo} className="app-section__logo" alt="logo" />
+        <div className="app-section__vertical-spacing" />
+        <div className="app-section__vertical-spacing" />
+        <div className="app-section__vertical-spacing" />
         <h1>
           Jon Hargreaves
         </h1>
@@ -43,7 +47,7 @@ function App() {
           <div className="app-section__horizontal-spacing" />
           <div className="app-section__column">
             <p>
-              I'm working remotely as a Senior Product Designer at <a href="https://quicknode.com/" target="_blank" className="app-section__yellow-link">QuickNode</a>. We make blockchain development simple and Web3 is powered by QuickNode.
+              I'm working remotely as a Senior Product Designer at <a href="https://quicknode.com/" target="_blank" className="app-section__yellow-link">QuickNode</a>. We make blockchain development simple and approachable for everyone. Web3 is powered by QuickNode.
               <br />
               <br />
               Freelancing status: <span>UNAVAILABLE</span>
@@ -68,33 +72,22 @@ function App() {
           <div className="app-section__horizontal-spacing" />
           <div className="app-section__column">
             <p>
-              01/2022 - 05/2022
+              <span>01/2022 - 05/2022</span>
               <br />
-              <a href="https://icy.tools/" target="_blank" className="app-section__red-link">Icy Tools</a>&nbsp;<span>(NFT analytics tools)</span>
-              <ul>
-                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
-                <li>Managed the design system + brand system</li>
-                <li>Coded marketing page animations</li>
-                <li>Acquired by QuickNode</li>
-              </ul>
+              Senior Product Designer at <a href="https://icy.tools/" target="_blank" className="app-section__red-link">Icy Tools</a>
               <br />
-              11/2020 - 01/2022
               <br />
-              <a href="https://apartment.app/" target="_blank" className="app-section__red-link">Apartment App</a>&nbsp;<span>(a Caretaker company for renters)</span>
-              <ul>
-                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
-                <li>Managed the design system + brand system</li>
-                <li>Coded marketing pages, components, & animations</li>
-              </ul>
+              <span>11/2020 - 01/2022</span>
               <br />
-              11/2016 - 01/2022
+              Head of Design at <a href="https://apartment.app/" target="_blank" className="app-section__red-link">Apartment App</a>
               <br />
-              <a href="https://caretaker.com/" target="_blank" className="app-section__red-link">Caretaker</a>&nbsp;<span>(automated property management suite)</span>
-              <ul>
-                <li>Designed the current logo, marketing pages, & new features throughout the product</li>
-                <li>Managed the design system + brand system</li>
-                <li>Coded marketing pages, components, & animations</li>
-              </ul>
+              (a Caretaker company)
+              <br />
+              <br />
+              <span>11/2016 - 01/2022</span>
+              <br />
+              Head of Design at <a href="https://caretaker.com/" target="_blank" className="app-section__red-link">Caretaker</a>
+              <br />
               <br />
               My career started as a freelance brand designer and illustrator. I was contracted and later hired by Caretaker (previously Flip) as a brand and product designer.
               As time went on, I learned front end development and started maintaining Caretaker's design system. I've realized that I'm a Design Generalist: a designer with many focuses.
@@ -116,14 +109,14 @@ function App() {
           <div className="app-section__horizontal-spacing" />
           <div className="app-section__column">
             <p>
-              I want to continue learning, mentor up-and-coming designers, and design more than just things that live on the internet. The possiblity of designing homes, furniture, houseware, and clothing is very exciting to me.
+              I want to continue learning and design more than just things that live on the internet. The possiblity of designing homes, furniture, houseware, and clothing is very exciting to me.
               <br />
               <br />
-              I would also love to continue growing my collection of modernist furniture, live in a mid century/modern home, find ways to build a more sustainable future with thoughtful design, and make good design accessible to everyone.
+              I would also love to continue growing my collection of modernist furniture, find ways to build a more sustainable future with thoughtful design, and make good design accessible to everyone.
               Most of all, I want to explore design the way <a href="https://www.rit.edu/vignellicenter/vignelli-legacy" target="_blank" className="app-section__blue-link">Massimo and Lella Vignelli</a> did.
               <br />
               <br />
-              I've never worked in an office and I don't intend to for the entirety of my career as a designer. I believe remote work is the future.
+              I've never worked in an office and I intend to work remotely for my entire design career.
               </p>
           </div>
           <div className="app-section__horizontal-spacing" />
@@ -171,8 +164,6 @@ function App() {
         <div className="app-section__vertical-spacing" />
         <div className="app-section__vertical-spacing" />
         <p><span>This website was designed and coded by Jon Hargeaves © 2022</span></p>
-        <div className="app-section__vertical-spacing" />
-        <img src={Logo} className="app-section__logo" alt="logo" />
       </div>
     </div>
   );
