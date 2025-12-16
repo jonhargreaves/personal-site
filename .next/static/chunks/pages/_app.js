9ea3fbb3351291f03c333030a1f278b4372f2c5d
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2025-12-15 21:49:13 -0500

    Updated Next + Node

--- .next/static/chunks/pages/_app.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/[root of the server]__180718._.js",
-  "static/chunks/545c3_react-dom_1c85ba._.js",
-  "static/chunks/node_modules__pnpm_f84813._.js",
-  "static/chunks/[root of the server]__f265a1._.js",
-  "static/chunks/pages__app_5771e1._.js",
-  "static/chunks/pages__app_d1ad5c._.js"
-])
