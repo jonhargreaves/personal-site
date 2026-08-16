5c3a788513c27c1b4cd082fdcaff8caaa53ddb39
Jon Hargreaves <jhargreavesdesigns@gmail.com>
2026-08-15 22:36:52 -0400

    Restore Connect menu text size

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -61,7 +61,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1 text-sm outline-none transition-colors focus:bg-white/10 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 motion-reduce:transition-none",
+      "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1 text-base outline-none transition-colors focus:bg-white/10 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 motion-reduce:transition-none",
       className,
     )}
     {...props}
